8ae1612c30fefe723a23661cfd3fa98e737bade1
Yana Yapici <yana.yapici@zhdk.ch>
2025-02-08 18:50:22 +0100

    init movement setup

---
+++ NJL/Assets/_Scripts/StateMachine/PlayerController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    PlayerInput playerInput;
+    CharacterController characterController;
+    void Start()
+    {
+        playerInput = new PlayerInput();
+
+    }
+
+
+}
