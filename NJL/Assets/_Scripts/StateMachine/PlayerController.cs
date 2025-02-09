e7f27287023bdb6b55e51603171a0e904af39e8b
Yana Yapici <yana.yapici@zhdk.ch>
2025-02-10 00:06:57 +0100

    base movement

--- NJL/Assets/_Scripts/StateMachine/PlayerController.cs
+++ NJL/Assets/_Scripts/StateMachine/PlayerController.cs
@@ -5,8 +5,12 @@ public class PlayerController : MonoBehaviour
     PlayerInput playerInput;
     [SerializeField]
     CharacterController characterController;
-    Vector3 moveDir = Vector3.zero;
-    float yAngle = 0;
+    Vector3 horVelocity = Vector3.zero;
+    Vector3 verVelocity = Vector3.zero;
+
+    [SerializeField]
+    float speed = 5;
+    float yLookAngle = 0;
 
     void Start()
     {
@@ -15,7 +19,7 @@ public class PlayerController : MonoBehaviour
         playerInput = new PlayerInput();
         playerInput.Enable();
         playerInput.Player.Walk.performed += ctx => MovePlayer(ctx.ReadValue<Vector2>());
-        playerInput.Player.Jump.performed += ctx => moveDir.y = 5;
+        playerInput.Player.Jump.performed += ctx => verVelocity.y = 5;
         playerInput.Player.Look.performed += ctx => LookAround(ctx.ReadValue<Vector2>());
     }
 
@@ -23,20 +27,22 @@ public class PlayerController : MonoBehaviour
     {
         if (!characterController.isGrounded)
         {
-            moveDir.y -= 9.8f * Time.deltaTime;
+            verVelocity.y += -9.8f * Time.deltaTime;
         }
-        characterController.Move(moveDir * Time.deltaTime);
+        characterController.Move(speed * Time.deltaTime * transform.TransformDirection(horVelocity));
+        characterController.Move(verVelocity * Time.deltaTime);
+
     }
 
     void MovePlayer(Vector2 direction)
     {
-        moveDir = new Vector3(direction.x, moveDir.z, direction.y);
+        horVelocity = new Vector3(direction.x, horVelocity.z, direction.y);
     }
 
     void LookAround(Vector2 direction)
     {
         transform.Rotate(Vector3.up * direction.x);
-        yAngle = Mathf.Clamp(yAngle - direction.y, -90, 90);
-        Camera.main.transform.localRotation = Quaternion.Euler(yAngle, 0, 0);
+        yLookAngle = Mathf.Clamp(yLookAngle - direction.y, -90, 90);
+        Camera.main.transform.localRotation = Quaternion.Euler(yLookAngle, 0, 0);
     }
 }
